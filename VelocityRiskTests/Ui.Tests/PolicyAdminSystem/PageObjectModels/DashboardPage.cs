a7c8be7ca242cb579209887f8dc1fe3582fb19ab
Anton Belakhvostau <anton.belakhvostov@ocsico.com>
2020-06-01 19:23:04 +0300

    added dashboard pageObject

--- VelocityRiskTests/Ui.Tests/PolicyAdminSystem/PageObjectModels/DashboardPage.cs
+++ VelocityRiskTests/Ui.Tests/PolicyAdminSystem/PageObjectModels/DashboardPage.cs
@@ -10,6 +10,9 @@ namespace Ui.Tests.PolicyAdminSystem.PageObjectModels
             PageUrl = "https://velocity-uat.britecorepro.com/agent/misc/ko_dashboard";
         }
 
+        public IWebElement SmallBusinessEmailLink => Driver.FindElement(By.XPath("//div[@id='page_container']//a[contains(text(), 'SMB.underwriting@velocityrisk.com')]"));
+        public IWebElement HereLink => Driver.FindElement(By.XPath("//a[contains(text(), 'here')]"));
+        public IWebElement HomeownersProductsEmailLink => Driver.FindElement(By.XPath("//div[@id='page_container']//a[@href='mailto:underwriting@velocityrisk.com']"));
 
     }
 }
