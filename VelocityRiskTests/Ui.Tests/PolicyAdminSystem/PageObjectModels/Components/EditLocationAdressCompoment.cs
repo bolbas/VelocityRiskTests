404b25b31c7481d2814e7b0f1e229e7f54153f6f
Viktoryia Suponenka <victoryia.suponenka@ocsico.com>
2020-06-04 12:56:54 +0300

    find element in parent element, fix locators for parent elements

--- VelocityRiskTests/Ui.Tests/PolicyAdminSystem/PageObjectModels/Components/EditLocationAdressCompoment.cs
+++ VelocityRiskTests/Ui.Tests/PolicyAdminSystem/PageObjectModels/Components/EditLocationAdressCompoment.cs
@@ -17,15 +17,15 @@ namespace Ui.Tests.PolicyAdminSystem.PageObjectModels.Components
         public IWebElement ZipCodeInput => Driver.FindElement(By.XPath("//input[@data-test-id='address-input-zip']"));
 
         //Has the insured had any property claims at this location in the past 5 years? Loss history is required for each location.
-        public IWebElement InsuredHadClaimsAtThisLocationInThePast5Years => Driver.FindElement(By.XPath("//span[contains(text(), 'Has the insured had any property claims at this location in the past 5 years? Loss history is required for each location.')]/ancestor::div[@class='bc-form__label c-builder-edit__question bc-form__label--has-validation']"));
-        public IWebElement InsuredHadClaimsAtThisLocationInThePast5YearsYesInput => Driver.FindElement(By.XPath("//input[@value ='Yes']"));
-        public IWebElement InsuredHadClaimsAtThisLocationInThePast5YearsNoInput => Driver.FindElement(By.XPath("//input[@value ='No']"));
+        public IWebElement InsuredHadClaimsAtThisLocationInThePast5Years => Driver.FindElement(By.XPath("//span[contains(text(), 'Has the insured had any property claims at this location in the past 5 years? Loss history is required for each location.')]/ancestor::div[contains(@class, 'bc-form__label')]"));
+        public IWebElement InsuredHadClaimsAtThisLocationInThePast5YearsYesInput => InsuredHadClaimsAtThisLocationInThePast5Years.FindElement(By.XPath("//input[@value ='Yes']"));
+        public IWebElement InsuredHadClaimsAtThisLocationInThePast5YearsNoInput => InsuredHadClaimsAtThisLocationInThePast5Years.FindElement(By.XPath("//input[@value ='No']"));
 
         //How many years has the applicant owned and operated this specific business?
-        public IWebElement YearsApplicantOwnedOperatedBusiness => Driver.FindElement(By.XPath("//span[contains(text(), 'How many years has the applicant owned and operated this specific business?')]/ancestor::div[@class='bc-form__label c-builder-edit__question bc-form__label--has-validation']"));
-        public IWebElement YearsApplicantOwnedOperatedBusinessLessThanYearInput => Driver.FindElement(By.XPath("//input[@value ='Less than a year']"));
-        public IWebElement YearsApplicantOwnedOperatedBusinessOneToThreeInput => Driver.FindElement(By.XPath("//input[@value ='1-3']"));
-        public IWebElement YearsApplicantOwnedOperatedBusinessMoreThanThree => Driver.FindElement(By.XPath("//input[@value ='More than 3']"));
+        public IWebElement YearsApplicantOwnedOperatedBusiness => Driver.FindElement(By.XPath("//span[contains(text(), 'How many years has the applicant owned and operated this specific business?')]/ancestor::div[contains(@class, 'bc-form__label')]"));
+        public IWebElement YearsApplicantOwnedOperatedBusinessLessThanYearInput => YearsApplicantOwnedOperatedBusiness.FindElement(By.XPath("//input[@value ='Less than a year']"));
+        public IWebElement YearsApplicantOwnedOperatedBusinessOneToThreeInput => YearsApplicantOwnedOperatedBusiness.FindElement(By.XPath("//input[@value ='1-3']"));
+        public IWebElement YearsApplicantOwnedOperatedBusinessMoreThanThree => YearsApplicantOwnedOperatedBusiness.FindElement(By.XPath("//input[@value ='More than 3']"));
 
         public IWebElement AdditionalLossInformationButton => Driver.FindElement(By.XPath("//button[contains(text(), 'Additional Loss Information')]"));
         public IWebElement AddMortgageeButton => Driver.FindElement(By.XPath("//button[@type='button' and contains(., 'Add a Mortgagee')]"));
